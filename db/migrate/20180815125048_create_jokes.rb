6723579de0054142aefd2c2d07505df0f7aecfe5
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-08-15 14:53:52 +0200

    add models

---
+++ db/migrate/20180815125048_create_jokes.rb
@@ -0,0 +1,10 @@
+class CreateJokes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :jokes do |t|
+      t.string :description
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_08_15_125153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "jokes", force: :cascade do |t|
+    t.string "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_jokes_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "joke_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["joke_id"], name: "index_likes_on_joke_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "pseudo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "jokes", "users"
+  add_foreign_key "likes", "jokes"
+  add_foreign_key "likes", "users"
 end
