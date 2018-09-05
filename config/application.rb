822122882c2375a0a15cb12982e98ee91c4602a1
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-09-05 17:22:46 +0200

    toujours en cours

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ Bundler.require(*Rails.groups)
 
 module Woodenspoon
   class Application < Rails::Application
-
     config.generators do |generate|
           generate.assets false
           generate.helper false
